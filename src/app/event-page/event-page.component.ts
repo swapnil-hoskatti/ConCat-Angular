362405cf83061beaca08d9a9b3ef8354f97fde85
Swapnil Hoskatti <shoskatti@gmail.com>
2022-05-06 04:30:19 +0530

    JSON data usage added
    Data has been moved to using JSON files. New Speaker information added. Page now redirects to main home is id is removed from URL. Additional Data such as Date, Time, etc. has also been added to the event-page.

--- src/app/event-page/event-page.component.ts
+++ src/app/event-page/event-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
+import data from '../../assets/data/events.json';
+import speakers from '../../assets/data/speakers.json';
 
 @Component({
   selector: 'app-event-page',
@@ -9,19 +11,44 @@ import { ActivatedRoute } from "@angular/router";
 
 export class EventPageComponent implements OnInit {
 
-  public page_title: any;
-  constructor(private route: ActivatedRoute) { }
-
+  public id: any;
+  public page_data: any;
+  public speaker_data: any;
+  public date_duration: any;
+  public to_date: any;
+  public from_date: any;
+  public time: any;
+  public start_time: any;
+  public end_time: any;
+  // constructor(private router: Router) { };
+  constructor(private route: ActivatedRoute,private router: Router) { };
+  
   ngOnInit() {
 
     this.route.queryParamMap.subscribe(params => {
-      const title = params.get('title');
-      this.page_title = title;
-      console.log(this.page_title);
+      const id = params.get('id');
+      this.id = id;
     });
 
-    console.log()
+    if (this.id) {
+    } else {
+      this.router.navigate(['']);
+    }
 
+    this.page_data = data[data.findIndex((x: { id: any; }) => x.id == this.id)]
+    this.speaker_data = speakers[data.findIndex((x: { id: any; }) => x.id == this.page_data.speaker_id)];
+    this.to_date = new Date(this.page_data.to_date);
+    this.from_date = new Date(this.page_data.from_date);
+    this.date_duration = this.to_date.getDate() - this.from_date.getDate() + 1;
+    this.start_time = this.page_data.from_time;
+    this.end_time = this.page_data.to_time;
+    let start_time_split = this.start_time.split(':');
+    let end_time_split = this.end_time.split(':');
+    let time_duration_mins = (end_time_split[0] - start_time_split[0]) * 60 + (end_time_split[1] - start_time_split[1]) + 30;
+    let hrs = Math.floor(time_duration_mins / 60);
+    let mins = time_duration_mins % 60;
+    this.time = hrs + 'hrs ' + mins + 'mins';
+    console.log(hrs, mins);
   }
 
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import data from '../../assets/data/events.json';
 
 @Component({
   selector: 'app-main',
@@ -6,31 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
-  data = [
-    {
-      title: 'Angular',
-      description: 'A powerful Javascript framework for building single page apps. Angular is open source, and maintained by Google.',
-      committee: 'CODEX',
-      img: 'assets/img/CodeX_logo.jpg'
-    },
-    {
-      title: 'React',
-      description: 'A JavaScript library for building user interfaces. React can be used as a base in the development of single page or mobile applications.',
-      committee: 'IEEE',
-      img: 'assets/img/IEEE_logo.jpg'
-    },
-    {
-      title: 'Vue',
-      description: 'A progressive Javascript framework for building user interfaces. Vue can be used as a base in the development of single page or mobile applications.',
-      committee: 'CODEX',
-      img: 'assets/img/CodeX_logo.jpg'
-    }
-  ]
-
+  data = data;
+  
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.data);
   }
 
 }
